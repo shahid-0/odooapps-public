7147ee313f8c63f7746fc0ed56e037be533d3dc7
Shahid Khan <shahid.khannn66@gmail.com>
2025-07-27 17:40:23 +0500

    [ADD] create pos_product_stock_info module

---
+++ pos_product_stock_info/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "POS Product Stock Info",
+    'summary': "Hide out-of-stock products and show stock on POS",
+    'description': """""",
+    'author': "Shahid Khan",
+    'website': "https://apps.odoo.com/apps/modules/browse?search=Shahid+Khan",
+    'category': 'Point of Sale',
+    'version': '18.0.1.0.0',
+    'depends': ["point_of_sale"],
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    'demo': [],
+    'license': 'LGPL-3',
+}
+
